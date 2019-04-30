469c5d3033c5d60c5e2567b08d72bfede8cf35b5
Brandon Lewien <33644290+brandonlewien@users.noreply.github.com>
2019-04-30 01:35:50 -0600

    Update test.s

--- Project2/test.s
+++ Project2/test.s
@@ -6,7 +6,7 @@
 DELAY:
 	movia   r22, 0x08D7840
 	addi    r23, r23, 0x01              # Counter, reset is below every time this delay is called
-	bne	    r22, r23, DELAY             # If statement
+	bne	r22, r23, DELAY             # If statement
 	ret
 CONDITION:
 	cmpeqi 	r15, r16, 1                 # If Flag and 1, set r15 as true, else false, toggle basically
@@ -22,32 +22,32 @@ FORWARD:
 	ldw     r7, 0(r5)                   # Load new information from r5 to r7
 	slli 	r6, r6, 8                   # Shift left 8, useful when loading new things
 	add 	r6, r6, r7                  # Add 'catcher' register to output register
-	stwio   r6, (r3)					# Display the state on segments
+	stwio   r6, (r3)		    # Display the state on segments
 	addi    r5, r5, 4                   # Shift register by 4
-	call 	DELAY						# Call to Delay above, will return here
+	call 	DELAY			    # Call to Delay above, will return here
 	movui   r23, 0x0                    # Reset counter register within delay
-	movi    r16, 1						# Flag setter
+	movi    r16, 1			    # Flag setter
 	beq     r6, r20, SKIPQUEUE          # Branch if 0x00000000 for reset purposes
-	br 		LOOP						# Otherwise go to loop directly
+	br 	LOOP			    # Otherwise go to loop directly
 BACKWARD:
 	beq     r16, r9, BFOR
 	ldw     r12, 0(r10)                 # Load new information from r10 to r12
-	slli    r12, r12, 24				# Shift logical left by 24 bits to line up with leftmost edge
+	slli    r12, r12, 24		    # Shift logical left by 24 bits to line up with leftmost edge
 	srli 	r13, r13, 8                 # Shift right 8, useful when loading new things
 	add 	r13, r13, r12               # Add 'catcher' register to output register
-	stwio   r13, (r3)			    	# Display the state on segments
+	stwio   r13, (r3)	            # Display the state on segments
 	addi    r10, r10, 4                 # Shift register by 4
-	call 	DELAY						# Call to Delay above, will return here
-	movi    r16, 0					    # Flag setter
+	call 	DELAY			    # Call to Delay above, will return here
+	movi    r16, 0			    # Flag setter
 	movui   r23, 0x0                    # Reset counter register within delay
 	beq     r13, r20, SKIPQUEUE         # Branch if 0x00000000 for reset purposes
-	br	    LOOP                        # Otherwise go to loop directly
+	br	LOOP                        # Otherwise go to loop directly
 SKIPQUEUE:
 
 _start:
-	movia   r3, SEGMENT03				# Address of Segment 0-3
-	movia   r4, BUTTONS 				# Address of Both buttons                   
-	movia   r5, FW      				# Address of Forward Pattern
+	movia   r3, SEGMENT03		    # Address of Segment 0-3
+	movia   r4, BUTTONS 		    # Address of Both buttons                   
+	movia   r5, FW      		    # Address of Forward Pattern
 	movia   r10, BW                     # Address of Backward Pattern
 	movhi   r20, 0x0000                 # Store 0x0000 to high part of r20
 	addi    r20, r20, 0x0000            # Store 0x0000 to low part of r20
@@ -57,7 +57,7 @@ _start:
 LOOP:
 	ldwio	r8, 0(r4)                   # Read switch state
 	cmpeqi 	r18, r8, 1                  # Check if switch is pressed
-    beq     r18, r9, CONDITION	        # If switch is pressed, go to condition
+        beq     r18, r9, CONDITION	    # If switch is pressed, go to condition
 	beq 	r15, r0, FORWARD            # If r15 false, forward - will set flag to 1
 	beq     r15, r9, BACKWARD           # If r15 true, backward - will set flag to 0
 	br      LOOP                        # Repeat loop if branch criteria above not met
@@ -68,4 +68,4 @@ FW:
 BW:
 	.word 0b01001111, 0b01001001, 0b01001001, 0b01001001, 0b0, 0b0, 0b0, 0b0 
 
-    .end
+.end
